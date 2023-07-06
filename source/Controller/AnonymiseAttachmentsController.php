7c99a44293ae6322ce0a2c47848275418d62d83f
Zac Scott <zac@zacscott.net>
2023-07-06 20:27:55 +0000

    adjust image url rewrite logic

--- source/Controller/AnonymiseAttachmentsController.php
+++ source/Controller/AnonymiseAttachmentsController.php
@@ -12,17 +12,13 @@ class AnonymiseAttachmentsController {
 
     public function __construct() {
     
-        add_filter( 'wp_get_attachment_url', [ $this, 'rewrite_attachment_urls' ], PHP_INT_MAX, 2 );
-        add_filter( 'wp_get_attachment_image_src', [ $this, 'rewrite_image_urls' ], PHP_INT_MAX, 4 );
+        add_filter( 'wp_get_attachment_url', [ $this, 'wp_get_attachment_url' ], PHP_INT_MAX, 2 );
+        add_filter( 'wp_get_attachment_image_src', [ $this, 'wp_get_attachment_image_src' ], PHP_INT_MAX, 4 );
+        add_filter( 'wp_get_attachment_metadata', [ $this, 'wp_get_attachment_metadata' ], PHP_INT_MAX, 2 );
 
     }
 
-    public function rewrite_attachment_urls( $url, $attachment_id ) {
-
-        // Don't interfere with wp-admin.
-        if ( is_admin() ) {
-            return $url;
-        }
+    public function wp_get_attachment_url( $url, $attachment_id ) {
 
         if ( ! $this->lock ) {
             $this->lock = true;
@@ -36,12 +32,7 @@ class AnonymiseAttachmentsController {
 
     }
 
-    public function rewrite_image_urls( $image, $attachment_id, $size, $icon ) {
-
-        // Don't interfere with wp-admin.
-        if ( is_admin() ) {
-            return $image;
-        }
+    public function wp_get_attachment_image_src( $image, $attachment_id, $size, $icon ) {
 
         if ( ! $this->lock ) {
             $this->lock = true;
@@ -53,6 +44,18 @@ class AnonymiseAttachmentsController {
         }
 
         return $image;
+
+    }
+
+    public function wp_get_attachment_metadata( $data, $attachment_id ) {
+
+        $data['file'] = $attachment_id;
+
+        foreach ( $data['sizes'] as $size => $size_data ) {
+            $data['sizes'][$size]['file'] = $attachment_id;
+        }
+
+        return $data;
 
     }
 
