8c8ace478fc76952941b0955997d35b8dd49309d
Zac Scott <zac@zacscott.net>
2023-07-14 04:08:19 +0000

    add controller to disable WordPress core image resizing on upload

--- plugin.php
+++ plugin.php
@@ -16,4 +16,5 @@ define( 'JUST_FAST_IMAGES_PLUGIN_ABSURL', plugin_dir_url( __FILE__ )  );
 // Boot each of the plugin logic controllers.
 new \JustFastImages\Controller\RewriteAttachmentsController();
 new \JustFastImages\Controller\AssetRouteController();
+new \JustFastImages\Controller\DisableCoreImageResizingController();
 new \JustFastImages\Controller\SettingsController();
